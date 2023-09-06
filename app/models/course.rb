0de65fae028387e274aa101210f4d6247b10ceaa
Muhammad Abdullah <mabdullah5514@yahoo.com>
2023-09-06 17:37:09 +0500

    Work on the learning_path module

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,4 +1,5 @@
 class Course < ApplicationRecord 
     has_and_belongs_to_many :talents, join_table: 'courses_talents'
     belongs_to :instructor, polymorphic: true
+    has_and_belongs_to_many :learning_paths
 end
--- app/models/learning_path.rb
+++ app/models/learning_path.rb
@@ -1,3 +1,5 @@
 class LearningPath < ApplicationRecord
-    has_many :courses, { order(:position) }
+    has_many :courses,-> { order(:position) }
+    has_and_belongs_to_many :talents
+    has_and_belongs_to_many :courses
 end
