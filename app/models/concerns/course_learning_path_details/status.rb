2d4fb45ff456ec98d06b34388668474653a3e359
Muhammad Abdullah <mabdullah5514@yahoo.com>
2023-09-08 18:31:56 +0500

    worked on course completion

--- app/models/concerns/course_learning_path_details/status.rb
+++ app/models/concerns/course_learning_path_details/status.rb
@@ -27,7 +27,12 @@ module CourseLearningPathDetails
 
       def assign_next_course
         next_course = learning_paths.first.course_learning_path_details.where("course_position > ?",course_position).order(:course_position)
-        next_course.first.mark_as_inprogress!
+        if next_course.any?
+          next_course.first.mark_as_inprogress!
+        else
+          learing_path = LearningPahtTalent.where(talent_id: talent_id, learing_path_id: learning_paths.first.id)
+          learning_path.mark_as_completed!
+        end
       end
     end
   end
---
+++ app/models/concerns/learning_path_talents/status.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+module LearningPathTalents
+  module Status
+    extend ActiveSupport::Concern
+
+    included do 
+      include AASM
+      aasm column: 'status' do
+        state :inprogress, initial: true
+        state :completed
+        
+        event :mark_as_completed do
+          transitions from: :inprogress, to: :completed
+        end
+
+        event :mark_as_inprogress do
+          transitions from: :completed, to: :inprogress
+        end
+      end
+    end
+  end
+end
